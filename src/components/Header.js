29661d698c5c999bdbd317423151af321f05c7a0
sumanbhujel <xnamus@outlook.com>
2020-02-04 22:01:55 +0545

    browserrouter and home is created

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p>This is nav header</p>
+                <div>
+                    <NavLink to="/loginform">Login Form</NavLink>
+                    <NavLink to="/signupform">Sign Up Form</NavLink>
+                    <NavLink to="/addscrap">Add Scrap</NavLink>
+                    <NavLink to="/viewscrap">View Scrap</NavLink>
+                    <Button onClick={this.handleLogout}> Logout</Button>
+                </div>
+            </div>
+        )
+    }
+}
